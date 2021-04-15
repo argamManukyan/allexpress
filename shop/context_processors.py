e2f79195d8a26fb68adf9e02cbfbffced983f267
argamManukyan <manoukian00@gmail.com>
2021-04-15 10:56:55 +0400

    search-commit

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -2,4 +2,5 @@ from shop.models import Category
 
 def return_categories(request):
     categories_menu = Category.objects.all()
-    return {'categories_menu':categories_menu}
+    all_categories = Category.objects.all()
+    return {'categories_menu':categories_menu,'categories':all_categories}
